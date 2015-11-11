a57e0ad0906d45c7992452d250b45aaf03e6bd67
cliff0891 <yuyunc@student.swosu.edu>
2015-11-10 19:30:54 -0600

    started my code

---
+++ src/additionGameExtra/additiongameextra.java
@@ -0,0 +1,21 @@
+package additionGameExtra;
+
+import java.util.Scanner;
+
+public class additiongameextra {
+
+	
+	
+		// Below are variables used throughout all methods
+		public static int numberOfRounds       = 4;
+		public static int score 			   = 0;
+		public static int hardnessOfQuestion   = 10;
+		public static int correctAnswer;
+		public static Scanner input        = new Scanner (System.in);
+		
+		public static void main(String[] args) {
+		
+
+	}
+
+}
